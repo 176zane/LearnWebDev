7cf6182cbc31019d09e6d228c5a0ec0336699d40
姚七六 <176zane@gmail.com>
2019-01-11 00:29:52 +0800

    完善游戏逻辑

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -2,24 +2,64 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button className="square" onClick={()=>this.props.onClick()}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+//函数定义组件
+function Square(props) {
+  return (
+    //注意不能写成 onClick={props.onClick()} 否则 props.onClick 方法会在 Square 组件渲染时被直接触发而不是等到 Board 组件渲染完成时通过点击触发，又因为此时 Board 组件正在渲染中（即 Board 组件的 render() 方法正在调用），又触发 handleClick(i) 方法调用 setState() 会再次调用 render() 方法导致死循环。
+    <button className='square' onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
 
 class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
+  }
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if(calculateWinner(squares)||squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   render() {
-    const status = 'Next player: X';
-
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner) {
+      status = 'winner: ' + winner;
+    }else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    //render 返回的是一个 React 元素,通常使用 JSX 这种语法扩展来书写这种描述。
+    //我们在 JSX 元素的最外层套上一个小括号，以防止 JS 代码解析时自动在换行处添加分号。
     return (
       <div>
         <div className="status">{status}</div>
@@ -58,6 +98,27 @@ class Game extends React.Component {
     );
   }
 }
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 
 // ========================================
 
